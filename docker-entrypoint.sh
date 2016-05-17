68b5d352072dbf8260e1c4fe73ab108b9f03dc21
Slawek Kolodziej <hfrntt@gmail.com>
2016-05-17 02:31:21 +0200

    Bind to ec2 address in an easy way

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,4 +2,10 @@
 set -e
 
 export CONSUL_ADDRESS=`ip ro | grep default | awk '{print $3}'`:8500
+
+if [ "$BIND_TO_EC2" ]
+then
+	export REDIS_ADDRESS=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+fi
+
 exec /usr/local/bin/containerbuddy redis-server
